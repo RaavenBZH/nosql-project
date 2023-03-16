cbd03972394f554f60112fbf3a9d3308a24e3f3e
AlphardHydrae <sophie_bertrand@zohomail.eu>
2023-03-16 10:28:54 +0100

    [FIX] names

---
+++ collections/tracks.js
@@ -0,0 +1,28 @@
+const schema = {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      description: "collection",
+      required: ["country", "name", "distance"],
+      properties: {
+        _id: {
+          bsonType: "objectId",
+        },
+        country: {
+          bsonType: "string",
+          description: "must be a string and is required",
+        },
+        name: {
+          bsonType: "string",
+          description: "must be a string and is required",
+        },
+        distance: {
+          bsonType: "int",
+          description: "must be an integer and is required",
+        },
+      },
+    },
+  },
+};
+
+modeules.exports = schema;
--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 const mongo = require("mongodb").MongoClient;
 
-const collCircuit = require("./collections/circuit").schema;
-const documentCircuit = require("./insertions/circuit");
+const collDrivers = require("collections/drivers");
+const docDrivers = require("insertions/drivers");
+
+const collQualifiers = require("collections/qualifiers");
+const docQualifiers = require("insertions/qualifiers");
+
+const collRaces = require("collections/races");
+const docRaces = require("insertions/races");
+
+const collTracks = require("collections/tracks");
+const docTracks = require("insertions/tracks");
 
 mongo.connect("mongodb://localhost:27017").then(
   (client) => {
     console.log("connection success");
     let db = client.db("grandPrix");
 
-    let circuit = db.collection("circuit", collCircuit, (err) => {
-      console.log(err);
+    let tracks = db.collection("tracks", collTracks, (err) => {
+      console.error(err);
     });
 
-    circuit
-      .insertMany(documentCircuit, {
+    tracks
+      .insertMany(docTracks, {
         ordered: true,
       })
       .then(() => {
         console.log("insertion success");
       })
       .catch((err) => {
-        console.log(err);
+        console.error(err);
       });
   },
   (err) => {
-    console.log(err);
+    console.erro(err);
   }
 );
