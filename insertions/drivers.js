44f16bbceb2db71c3f852f717ed518d5b2149e80
RaavenBZH <emalivet56@gmail.com>
2023-03-21 18:22:20 +0100

    queries

--- insertions/drivers.js
+++ insertions/drivers.js
@@ -2,108 +2,135 @@ const insertion = [
   {
     lastName: "Verstappen",
     firstName: "Max",
+    team: "Red Bull",
     birthdate: "30-09-1997",
   },
   {
     lastName: "Perez",
     firstName: "Sergio",
+    team: "Red Bull",
     birthdate: "26-01-1990",
   },
   {
     lastName: "Leclerc",
     firstName: "Charles",
+    team: "Ferrari",
     birthdate: "16-10-1997",
   },
   {
     lastName: "Sainz",
     firstName: "Carlos",
+    team: "Ferrari",
     birthdate: "01-09-1994",
   },
   {
     lastName: "Hamilton",
     firstName: "Lewis",
+    team: "Mercedes",
     birthdate: "07-01-1985",
   },
   {
     lastName: "Russell",
     firstName: "George",
+    team: "Mercedes",
     birthdate: "15-02-1998",
   },
   {
     lastName: "Ocon",
     firstName: "Esteban",
+    team: "Alpine",
     birthdate: "17-09-1996",
   },
   {
     lastName: "Alonso",
     firstName: "Fernando",
+    team: "Alpine",
     birthdate: "29-07-1981",
   },
   {
     lastName: "Norris",
     firstName: "Lando",
+    team: "McLaren",
     birthdate: "13-11-1999",
   },
   {
     lastName: "Ricciardo",
     firstName: "Daniel",
+    team: "McLaren",
     birthdate: "01-07-1989",
   },
   {
     lastName: "Bottas",
     firstName: "Valtteri",
+    team: "Alfa Romeo",
     birthdate: "28-08-1989",
   },
   {
     lastName: "Zhou",
     firstName: "Guanyu",
+    team: "Alfa Romeo",
     birthdate: "30-05-1999",
   },
   {
     lastName: "Stroll",
     firstName: "Lance",
+    team: "Aston Martin",
     birthdate: "29-10-1998",
   },
   {
     lastName: "Vettel",
     firstName: "Sebastian",
+    team: "Aston Martin",
     birthdate: "03-07-1987",
   },
   {
     lastName: "Magnussen",
     firstName: "Kevin",
+    team: "Haas",
     birthdate: "05-10-1992",
   },
   {
     lastName: "Schumacher",
     firstName: "Mick",
+    team: "Haas",
     birthdate: "22-03-1999",
   },
   {
     lastName: "Gasly",
     firstName: "Pierre",
+    team: "Alpha Tauri",
     birthdate: "07-02-1996",
   },
   {
     lastName: "Tsunoda",
     firstName: "Yuki",
+    team: "Alpha Tauri",
     birthdate: "11-05-2000"
   },
   {
     lastName: "Latifi",
     firstName: "Nicholas",
+    team: "Williams",
     birthdate: "29-06-1995",
   },
   {
     lastName: "Albon",
     firstName: "Alexander",
+    team: "Williams",
     birthdate: "23-03-1996",
   },
   {
     lastName: "De Vries",
     firstName: "Nyck",
+    team: "Williams",
     birthdate: "06-02-1995",
   },
+  {
+    lastName: "Hulkenberg",
+    firstName: "Nico",
+    team: "Aston Martin",
+    birthdate: "19-08-1987",
+  }
 ];
 
 module.exports = insertion;
--- queries.js
+++ queries.js
@@ -17,14 +17,14 @@ mongo.connect("mongodb://localhost:27017").then(
           }
         });
       })
-    // Result
-    .then(
-      () => {
-        console.log("Combien de pilotes différents ont gagné une course ?")
-        console.log(">>> " + count);
-        console.log(">>> " + memo + "\n");
-      }
-    );
+      // Result
+      .then(
+        () => {
+          console.log("Q1 : Combien de pilotes différents ont gagné une course ?");
+          console.log(`>>> ${count} (${memo})`);
+          console.log();
+        }
+      );
   }
 );
 
@@ -43,13 +43,14 @@ mongo.connect("mongodb://localhost:27017").then(
         });
       })
 
-    // Result
-    .then(
-      () => {
-        console.log("Quelle est la longueur moyenne d'un circuit en 2022 (en mètres) ?");
-        console.log(">>> " + (sum/count).toFixed(2) + "m\n");
-      }
-    );
+      // Result
+      .then(
+        () => {
+          console.log("Q2 : Quelle est la longueur moyenne d'un circuit en 2022 (en mètres) ?");
+          console.log(">>> " + (sum / count).toFixed(2) + "m");
+          console.log();
+        }
+      );
   }
 );
 
@@ -91,14 +92,162 @@ mongo.connect("mongodb://localhost:27017").then(
         }
       })
 
-    // Result
-    .then(
-      () => {
-        console.log("Quel pilote a obtenu le plus de pôles positions (1er en qualifications) ?");
-        console.log(">>> " + count);
-      }
-    );
+      // Result
+      .then(
+        () => {
+          console.log("Q3 : Quel pilote a obtenu le plus de pôles positions (1er en qualifications) ?");
+          console.log(">>> " + count);
+          console.log();
+        }
+      );
   }
 );
 
 // Q4
+mongo.connect("mongodb://localhost:27017").then(
+  (client) => {
+    let db = client.db("f1");
+
+    var count = 0;
+    db.collection('races').find({}).toArray().then(
+      (docs) => {
+        docs.forEach((doc) => {
+          var podium = doc.standings.slice(0, 3);
+          podium.forEach((driver) => {
+            if (driver.team == "Ferrari") {
+              count++;
+            }
+          });
+        });
+      })
+
+      // Result
+      .then(
+        () => {
+          console.log("Q4 : Combien de podiums l'équipe Ferrari a-t-elle obtenu ?");
+          console.log(">>> " + count);
+          console.log();
+        }
+      );
+  }
+);
+
+// Q5
+mongo.connect("mongodb://localhost:27017").then(
+  (client) => {
+    let db = client.db("f1");
+
+    var ages = [];
+    db.collection('drivers').find({}).toArray().then(
+      (docs) => {
+        docs.forEach((doc) => {
+          // traite la date de naissance
+          var birthdate = doc.birthdate.split("-");
+          var date = new Date(birthdate[2], birthdate[1] - 1, birthdate[0]);
+
+          // calcule l'âge
+          var diff = Date.now() - date.getTime();
+          var age_date = new Date(diff);
+          var age = Math.abs(age_date.getUTCFullYear() - 1970);
+
+          ages.push(age);
+        });
+      })
+
+      // Result
+      .then(
+        () => {
+          console.log("Q5 : Quel est l'âge moyen des pilotes ?");
+          console.log(">>> " + (ages.reduce((a, b) => a + b) / ages.length).toFixed(2) + " ans");
+        }
+      );
+  }
+);
+
+// Q6 : Modifier les données pour que l'année soit 2021
+mongo.connect("mongodb://localhost:27017").then(
+  (client) => {
+    let db = client.db("f1");
+
+    db.collection("qualifyings").updateMany(
+      { year: 2022 },
+      { $set: { year: 2021 } }
+    );
+
+    db.collection("races").updateMany(
+      { year: 2022 },
+      { $set: { year: 2021 } }
+    );
+
+    db.collection("sprints").updateMany(
+      { year: 2022 },
+      { $set: { year: 2021 } }
+    );
+
+    db.collection("seasons").updateMany(
+      { year: 2022 },
+      { $set: { year: 2021 } }
+    );
+
+    console.log("Q6 : Modification des données pour que l'année soit 2021");
+  }
+);
+
+// Q7 : Échanger l'équipe des pilotes Hamilton et Verstappen. // NE FONCTIONNE PAS
+mongo.connect("mongodb://localhost:27017").then(
+  (client) => {
+    let db = client.db("f1");
+
+    var hamiltonTeam = "";
+    var verstappenTeam = "";
+    db.collection("drivers").find(
+      { lastName: { $in: ["Hamilton", "Verstappen"] } }
+    ).toArray().then(
+      (docs) => {
+        var h = "";
+        var v = "";
+        docs.forEach((doc) => {
+          if (doc.lastName == "Hamilton") {
+            h = doc.team;
+          } else {
+            v = doc.team;
+          }
+        });
+        hamiltonTeam = h;
+        verstappenTeam = v;
+      }
+    )
+      .then(() => {
+
+        var collections = ["races", "qualifyings", "sprints"]
+
+        // Verstappen -> Hamilton
+        db.collection("drivers").updateOne(
+          { lastName: "Verstappen" },
+          { $set: { team: hamiltonTeam } }
+        );
+
+        collections.forEach((collection) => {
+          db.collection(collection).updateMany(
+            { "standings.driver": "Verstappen" },
+            { $set: { "standings.$.team": hamiltonTeam } }
+          );
+        });
+
+        // Hamilton -> Verstappen
+
+        db.collection("drivers").updateMany(
+          { lastName: "Hamilton" },
+          { $set: { team: verstappenTeam } }
+        );
+
+        var collections = ["races", "qualifyings", "sprints"]
+        collections.forEach((collection) => {
+          db.collection(collection).updateMany(
+            { "standings.driver": "Hamilton" },
+            { $set: { "standings.$.team": verstappenTeam } }
+          );
+        });
+      })
+  }
+);
