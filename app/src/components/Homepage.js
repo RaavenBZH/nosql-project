b55cb6d792d7eb2da150610a394ce533a5e105c0
AlphardHydrae <sophie_bertrand@zohomail.eu>
2023-03-30 18:37:55 +0200

    [NEW] Homepage

--- app/src/components/Homepage.js
+++ app/src/components/Homepage.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Carousel from "react-bootstrap/Carousel";
+import Card from "react-bootstrap/Card";
+
+import { MDBIcon, MDBTypography } from "mdb-react-ui-kit";
+import ReactPlayer from "react-player";
 
 import drivers from "../assets/img/drivers.jpg";
 import qualifyings from "../assets/img/qualifyings.jpg";
@@ -19,99 +27,137 @@ export default function Homepage() {
 
   return (
     <main>
-      <Carousel fade>
-        <Carousel.Item interval={5000}>
-          <img
-            className="d-block w-100"
-            src={slide1}
-            height="800px"
-            alt="Slide 1"
-          />
-          <Carousel.Caption>
-            <h3>Slide 1</h3>
-            <p>Some text</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item interval={5000}>
-          <img
-            className="d-block w-100"
-            src={slide2}
-            height="800px"
-            alt="Slide 2"
-          />
-          <Carousel.Caption>
-            <h3>Slide 2</h3>
-            <p>Some text</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item interval={5000}>
-          <img
-            className="d-block w-100"
-            src={slide3}
-            height="800px"
-            alt="Slide 3"
-          />
-          <Carousel.Caption>
-            <h3>Slide 3</h3>
-            <p>Some text</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+      <section>
+        <Carousel fade>
+          <Carousel.Item interval={5000}>
+            <img
+              className="d-block w-100"
+              src={slide1}
+              height="100%"
+              alt="Slide 1"
+            />
+          </Carousel.Item>
+          <Carousel.Item interval={5000}>
+            <img
+              className="d-block w-100"
+              src={slide2}
+              height="100%"
+              alt="Slide 2"
+            />
+          </Carousel.Item>
+          <Carousel.Item interval={5000}>
+            <img
+              className="d-block w-100"
+              src={slide3}
+              height="100%"
+              alt="Slide 3"
+            />
+          </Carousel.Item>
+        </Carousel>
+      </section>
 
-      <div class="p-3 m-3">
-        <div class="container text-center">
-          <div class="row row-cols-sm-2 d-flex align-items-center g-3">
-            <div class="col-sm-6">
-              <div class="card" onClick={() => goTo("drivers")}>
-                <img src={drivers} class="card-img" alt="drivers" />
-                <div class="card-img-overlay d-flex justify-content-center align-items-center">
-                  <h5 class="card-title text-white">Drivers</h5>
-                </div>
-              </div>
-            </div>
-            <div class="col-sm-6">
-              <div class="card" onClick={() => goTo("qualifyings")}>
-                <img src={qualifyings} class="card-img" alt="qualifyings" />
-                <div class="card-img-overlay d-flex justify-content-center align-items-center">
-                  <h5 class="card-title text-white">Qualifyings</h5>
-                </div>
-              </div>
-            </div>
-            <div class="col-sm-6">
-              <div class="card" onClick={() => goTo("races")}>
-                <img src={races} class="card-img" alt="races" />
-                <div class="card-img-overlay d-flex justify-content-center align-items-center">
-                  <h5 class="card-title text-white">Races</h5>
-                </div>
-              </div>
-            </div>
-            <div class="col-sm-6">
-              <div class="card" onClick={() => goTo("seasons")}>
-                <img src={seasons} class="card-img" alt="seasons" />
-                <div class="card-img-overlay d-flex justify-content-center align-items-center">
-                  <h5 class="card-title text-white">Seasons</h5>
-                </div>
-              </div>
-            </div>
-            <div class="col-sm-6">
-              <div class="card" onClick={() => goTo("sprints")}>
-                <img src={sprints} class="card-img" alt="sprints" />
-                <div class="card-img-overlay d-flex justify-content-center align-items-center">
-                  <h5 class="card-title text-white">Sprints</h5>
-                </div>
-              </div>
-            </div>
-            <div class="col-sm-6">
-              <div class="card" onClick={() => goTo("tracks")}>
-                <img src={tracks} class="card-img" alt="tracks" />
-                <div class="card-img-overlay d-flex justify-content-center align-items-center">
-                  <h5 class="card-title text-white">Tracks</h5>
-                </div>
-              </div>
-            </div>
-          </div>
+      <section className="py-5 h-100">
+        <Container>
+          <Row className="justify-content-center align-items-center h-100">
+            <Col sm={12}>
+              <figure
+                className="bg-light p-4"
+                style={{
+                  borderLeft: ".35rem solid #ce2e1e",
+                  borderTop: "1px solid #eee",
+                  borderRight: "1px solid #eee",
+                  borderBottom: "1px solid #eee",
+                }}
+              >
+                <MDBIcon
+                  fas
+                  icon="quote-left mb-4"
+                  size="2x"
+                  style={{ color: "#ce2e1e" }}
+                />
+                <MDBTypography blockquote>
+                  <p className="pb-2">
+                    You never stop learning in F1. It's the typical thing that
+                    all drivers say, but it's absolutely true. But also, apart
+                    from driving, you learn a bit about the political side of
+                    F1. People don't realise how much there is outside the car.
+                  </p>
+                </MDBTypography>
+                <figcaption className="blockquote-footer mb-0">
+                  Carlos Sainz Jr.
+                </figcaption>
+              </figure>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <section>
+        <Container class="text-center">
+          <Row xs={1} md={2} className="align-items-center g-3">
+            <Col>
+              <Card onClick={() => goTo("drivers")}>
+                <Card.Img src={drivers} alt="drivers" />
+                <Card.ImgOverlay className="d-flex justify-content-center align-items-center">
+                  <Card.Title className="text-white fs-1">Drivers</Card.Title>
+                </Card.ImgOverlay>
+              </Card>
+            </Col>
+            <Col>
+              <Card onClick={() => goTo("qualifyings")}>
+                <Card.Img src={qualifyings} alt="qualifyings" />
+                <Card.ImgOverlay className="d-flex justify-content-center align-items-center">
+                  <Card.Title className="text-white fs-1">
+                    Qualifyings
+                  </Card.Title>
+                </Card.ImgOverlay>
+              </Card>
+            </Col>
+            <Col>
+              <Card onClick={() => goTo("races")}>
+                <Card.Img src={races} alt="races" />
+                <Card.ImgOverlay className="d-flex justify-content-center align-items-center">
+                  <Card.Title className="text-white fs-1">Races</Card.Title>
+                </Card.ImgOverlay>
+              </Card>
+            </Col>
+            <Col>
+              <Card onClick={() => goTo("seasons")}>
+                <Card.Img src={seasons} alt="seasons" />
+                <Card.ImgOverlay className="d-flex justify-content-center align-items-center">
+                  <Card.Title className="text-white fs-1">Seasons</Card.Title>
+                </Card.ImgOverlay>
+              </Card>
+            </Col>
+            <Col sm={6}>
+              <Card onClick={() => goTo("sprints")}>
+                <Card.Img src={sprints} alt="sprints" />
+                <Card.ImgOverlay className="d-flex justify-content-center align-items-center">
+                  <Card.Title className="text-white fs-1">Sprints</Card.Title>
+                </Card.ImgOverlay>
+              </Card>
+            </Col>
+            <Col>
+              <Card onClick={() => goTo("tracks")}>
+                <Card.Img src={tracks} alt="tracks" />
+                <Card.ImgOverlay className="d-flex justify-content-center align-items-center">
+                  <Card.Title className="text-white fs-1">Tracks</Card.Title>
+                </Card.ImgOverlay>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <section className="w-100 p-5 mt-5 bg-dark">
+        <h1 className="card-title pb-5 fs-1 text-center text-white">
+          Instant Replays
+        </h1>
+        <div className=" d-flex justify-content-around align-items-center">
+          <ReactPlayer url="https://www.youtube.com/watch?v=7YMjw2sjXqU" />
+          <ReactPlayer url="https://www.youtube.com/watch?v=7YMjw2sjXqU" />
         </div>
-      </div>
+      </section>
     </main>
   );
 }
